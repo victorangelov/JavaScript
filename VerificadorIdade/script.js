83d1c66f108653a40f8d59ed10b2489470558ac4
Victor Silva <victor_angelo@hotmail.com>
2021-02-20 15:16:06 -0300

    submmit imagens
    submmit imagens

---
+++ VerificadorIdade/script.js
@@ -0,0 +1,24 @@
+//let nascimento = document.getElementById('nascimentotxt')
+//let img = document.getElementById('txtimg')
+//let idade = document.getElementById('txtidade')
+
+//let data = new Date()
+//let ano = data.getFullYear()
+txtverificar.addEventListener('click', verificar)
+function verificar(){
+    let nascimento = document.getElementById('txtnascimento')
+    let sexo = document.getElementById()
+    let res = document.getElementById('txtidade')
+}
+//function verificar(){
+// nascimento -= ano
+//}
+
+
+
+
+
+//teste
+//idade.innerHTML = nascimento
+//teste
+//img.src = 'img/buceta.png'
