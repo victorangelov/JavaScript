ef89a39d2772f988a9bec3e2a806c89a8dc628a5
Victor Silva <victor_angelo@hotmail.com>
2021-02-20 14:18:56 -0300

    submmit projeto hora do dia concluido
    submmit projeto hora do dia concluido

--- HoraDoDia/script.js
+++ HoraDoDia/script.js
@@ -1,11 +1,30 @@
-let div = document.querySelector('div#txthora')
-let div2 = document.querySelector('div#txtimg')
-
-let agora = new Date()
-hora = agora.getHours()
-div.innerHTML = `Agora são ${hora} hora(s)`
-
-if (hora > 12){
-    document.style.background = 'white'
+function carregar(){
+    let div = document.querySelector('div#txthora')
+    let img = document.getElementById('imagem')
+    let div2 = document.querySelector('div#txtimg')
+    
+    let agora = new Date()
+    hora = agora.getHours()
+    //hora = 19 // para teste 
+    div.innerHTML = `Agora são ${hora} hora(s)`
+    
+    if (hora < 12){
+        //bom dia
+        img.src = 'img/manha.png'
+        div2.innerHTML += `<br>Bom dia!`
+        document.body.style.background = 'rgb(132, 148, 168)'
+        
+    } else if (hora < 18){
+        //boa tarde
+        img.src = 'img/tarde.png'
+        div2.innerHTML += `<br><br>Boa tarde!`
+        document.body.style.background = 'rgb(195, 204, 113);'
+        X
+    } else {
+        //boa noite
+        img.src = 'img/noite.png'
+        div2.innerHTML += `<br>Boa noite!`
+        document.body.style.background = 'black'
+        
+    }
 }
-
