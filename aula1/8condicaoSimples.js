90b421253ee8ca373df44fc1583712197a6ad24b
Victor Silva <victor_angelo@hotmail.com>
2021-02-19 22:44:12 -0300

    submit ultima aula
    submit ultima aula

---
+++ aula1/8condicaoSimples.js
@@ -0,0 +1,10 @@
+console.log('O console funcinou corretamente!')
+let vel = 60.5
+
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+console.log(`Diriga sempre utilizando cinto de segurança`)
+if (vel >= 60){
+    console.log(`Você ultrapassou a velocidade máxima, foi multado`)
+} else{
+    console.log(`Você está dento da velocidade permitida`)
+}
---
+++ aula1/9condicaoComposta.js
@@ -0,0 +1,8 @@
+let pais = 'Brasil'
+pais.toUpperCase()
+console.log(`Vivendo em ${pais}`)
+if (pais == 'BRASIL'){
+    console.log(`Você é Brasileiro`)
+} else {
+    console.log(`Você é Estrangeiro`)
+}
