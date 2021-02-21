44aed30f1d5efc3ec263ea4a5ab895fec7fffb06
Victor Silva <victor_angelo@hotmail.com>
2021-02-21 11:49:09 -0300

    projeto finalizado.
    projeto finalizado.

--- VerificadorIdade/script.js
+++ VerificadorIdade/script.js
@@ -10,9 +10,9 @@ function verificar(){
     // VARIÁVEL DA RESPOSTA
     let res = document.querySelector('div#txtres')
     // variável para imagem do html
-    let img = document.querySelector('img#txtimg')
     
-    
+    // definindo variável do elemento img do html
+    //let img = document.querySelector('img#txtimg')
     
     //VALIDANDO SE O CAMPO ESTÁ PREENCHIDO E SE A DATA NÃO É MAIOR QUE O ANO ATUAL
     if (fano.value.length == 0 || Number(fano.value) > ano){
@@ -21,13 +21,57 @@ function verificar(){
           var fsex = document.getElementsByName('radsex')
           var idade = ano - Number(fano.value)  
           var genero = ''
-          img.src = 'img/menino.png'  
+          //criando elemento img dentro do html dinamicamente
+          let img = document.createElement('img')
+          img.setAttribute('id','foto')
+          //alterando imagem que já está no HTML
+          //img.src += 'img/menino.png'  
           if (fsex[0].checked){
               genero = 'Masculino'
+              if (idade < 5){
+                //setando abritubo no elemento img dinamicamente
+                img.setAttribute('src', 'img/bbm.png')
+                  //bb
+              } else if (idade < 12){
+                    img.setAttribute('src', 'img/menino.png')
+                    //menino
+              }else if(idade < 19) {
+                  img.setAttribute('src', 'img/jovemh.png')
+                  //jovem
+              }else if (idade < 59){
+                  img.setAttribute('src', 'img/homem.png')
+                  //homem
+              }else if (idade < 100){
+                  img.setAttribute('src', 'img/velho.png')
+                  //velho
+              }else {
+                  img.setAttribute('src', 'img/homemmaisvelho.png')
+                  // bem velho
+              }
           } else {
               genero = 'Feminino'
+              if (idade < 5){
+                //setando abritubo no elemento img dinamicamente
+                img.setAttribute('src', 'img/bbf.png')
+                  //bb
+              } else if (idade < 12){
+                    img.setAttribute('src', 'img/menina.png')
+                    //menino
+              }else if(idade < 19) {
+                  img.setAttribute('src', 'img/jovemm.png')
+                  //jovem
+              }else if (idade < 59){
+                  img.setAttribute('src', 'img/mulher.png')
+                  //homem
+              }else if (idade < 100){
+                  img.setAttribute('src', 'img/velha.png')
+                  //velho
+              }else {
+                  img.setAttribute('src', 'img/mulhermaisvelha.png')
+                  // bem velho
+              }
           }
           res.innerHTML = `Detectamos pessoa de gênero ${genero} com ${idade} anos`
-           
+          res.appendChild(img)      
     }
 }
