3e40134312808dcd9adedf4ecabb3568685b29b7
Victor Silva <victor_angelo@hotmail.com>
2021-02-20 23:14:17 -0300

    shit
    shit

--- VerificadorIdade/script.js
+++ VerificadorIdade/script.js
@@ -9,30 +9,25 @@ function verificar(){
     let fano = document.getElementById('txtano')
     // VARIÁVEL DA RESPOSTA
     let res = document.querySelector('div#txtres')
-
+    // variável para imagem do html
+    let img = document.querySelector('img#txtimg')
+    
+    
+    
     //VALIDANDO SE O CAMPO ESTÁ PREENCHIDO E SE A DATA NÃO É MAIOR QUE O ANO ATUAL
     if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert(`[ERRO] Verifique os dados e tente novamente`)
     } else {
-        // validando GÊNERO
-        let fsex = document.getElementsByName('radsex')
-    
-        let idade = ano - Number(fano.value)
-        let genero = ''
-        //criando elemento imagem no JS
-        let img = document.createElement('img')
-        //setando id foto na tag img criada
-        img.setAttribute('id','foto')
-
-        if (fsex[0].checked) {
-            genero = 'Homem'
-            img.src = 'homem.png'
-        } else if (fsex[1].checked){
-            genero = 'Mulher'
-            img.src = 'mulher.png'
-        }
-        // alinhando texto com JS dinamicamente
-        res.style.textAlign = 'center'
-        res.innerHTML += `detectamos ${genero} com ${idade} anos`
+          var fsex = document.getElementsByName('radsex')
+          var idade = ano - Number(fano.value)  
+          var genero = ''
+          img.src = 'img/menino.png'  
+          if (fsex[0].checked){
+              genero = 'Masculino'
+          } else {
+              genero = 'Feminino'
+          }
+          res.innerHTML = `Detectamos pessoa de gênero ${genero} com ${idade} anos`
+           
     }
 }
