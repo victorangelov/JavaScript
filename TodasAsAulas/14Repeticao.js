57331cf80fd0e20af9832bab6ec22510b7ca2085
Victor Silva <victor_angelo@hotmail.com>
2021-02-21 22:22:57 -0300

    repetição aula
    repetição aula

---
+++ TodasAsAulas/14Repeticao.js
@@ -0,0 +1,18 @@
+
+let pizza = 5
+do {
+    
+    console.log(`vai se fuder ${pizza}`)
+    pizza--
+} while ( pizza > 0)
+
+pizza = 5
+while(pizza > 0){
+    console.log(`vai você ${pizza}`)
+    pizza--
+}
+
+pizza = 5
+for(let i = 0; i < pizza; i++){
+    console.log(`eu não ${pizza}`)
+} 
