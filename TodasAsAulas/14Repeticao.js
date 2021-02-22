51b43017f4c48c4683f4da53b6d820cc6a44265f
Victor Silva <victor_angelo@hotmail.com>
2021-02-21 22:57:19 -0300

    debug teste
    debug teste

--- TodasAsAulas/14Repeticao.js
+++ TodasAsAulas/14Repeticao.js
@@ -1,17 +1,18 @@
 
 let pizza = 5
+//teste lógico no início
 do {
-    
     console.log(`vai se fuder ${pizza}`)
     pizza--
 } while ( pizza > 0)
 
 pizza = 5
+//teste lógico no final
 while(pizza > 0){
     console.log(`vai você ${pizza}`)
     pizza--
 }
-
+// estrutura de repetição com variável de controle
 pizza = 5
 for(let i = 0; i < pizza; i++){
     console.log(`eu não ${pizza}`)
---
+++ TodasAsAulas/15Debug.js
@@ -0,0 +1,5 @@
+console.log('VAI COMEÇAR')
+for(let i = 1; i < 5; i++){
+    console.log(i)
+}
+console.log('FIM')
