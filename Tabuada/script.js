ea20abac1c8f1ed193909603bf85cc57a6849570
Victor Silva <victor_angelo@hotmail.com>
2021-02-22 22:16:06 -0300

    tabuada foi fácil
    tabuada foi fácil

--- Tabuada/script.js
+++ Tabuada/script.js
@@ -1,3 +1,18 @@
-let res = document.querySelector('div#txtres')
+txtgerar.addEventListener('click', gerar)
 
-res.innerHTML = `Teste do script`
+function gerar(){
+    let res = document.querySelector('div#txtres') 
+    let num = document.querySelector('input#txtnum')
+    
+    res.innerHTML = ''
+
+    if(num.value.length == 0){
+        window.alert(`Por favor digite um número antes de gerar tabuada`)
+    } else {
+        let numero = Number(num.value)
+        for(let i = 0; i <= 10; i++){
+            res.innerHTML += `${numero} x ${i} = ${i*numero} <br>`
+        }
+        
+    }     
+}
