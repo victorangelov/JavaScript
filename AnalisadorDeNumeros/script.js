799e87ae43179502a78f58fb3e42a7ff57a132dd
Victor Ângelo Sánchez da Silva <victor.alucard@gmail.com>
2021-02-24 14:33:17 -0300

    commit adicionarMais
    commit adicionarMais

--- AnalisadorDeNumeros/script.js
+++ AnalisadorDeNumeros/script.js
@@ -1,32 +1,62 @@
-let num = document.querySelector('input#txtnum')
+let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
-let res = document.querySelector('div#txtres')
+let res = document.querySelector('div#res')
 let valores = []
 
-function isNumero(numero){
-    if(Number(numero) >= 1 && Number(numero) != null){
+//retorna se o campo foi preenchido de for entre 1 e 100
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
         return true
     } else {
         return false
     }
 }
 
-function inLista(numero, lista){
-    if(numero.indexOf != -1){
+//retorna se o valor não é encontrado na lista
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1){
         return true
     } else {
         return false
     }
 }
-
+//chamada pelo botão Adicionar do HTML
 function adicionar(){
+    //testa se o número está entre 1 e 100 e se o valor está dentro do vetor
     if(isNumero(num.value) && !inLista(num.value, valores)){
-
+        //tem que definir como number se não ele 
+        //sempre vai ser reconhecido como se não estivesse no vetor
+        valores.push(Number(num.value))
+        window.alert(`valor ${num.value} adicionado ao vetor`)
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
 }
 
+function finalizar(){
+    
+}
 
+function eNumero(n){
+    if (n < 1 || n > 100){
+        return true
+    } else {
+        return false
+    }
+}
 
+function taLista(n, l){
+    if (l.indexOf(Number(n.value) != -1)){
+        return true 
+    } else {
+        return false
+    }
+}
 
+function adicionarMais(){
+    if(eNumero(num.value) && taLista(num.value, valores){
+        window.alert('Valor inválido ou não encontrado na lista')
+    } else {
+
+    }
+}
